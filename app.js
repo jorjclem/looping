aa1411aea5f51f91d63f2254e85512e373df0879
jorjclem <97485184+jorjclem@users.noreply.github.com>
2022-02-07 04:17:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+let number= 0;
+
+//for(let i=0;i<6; i++){
+    //number=number+2;
+//}
+
+//while(start<4){
+    //number=number+2;
+    //start++;
+//}
+
+while(answer!=="*"){
+    answer=prompt("what symbol do you want?")
+}
+
+document.body.innerhtml=number;
